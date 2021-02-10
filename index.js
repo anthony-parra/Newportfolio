42ce6e8be3b3da1196f1d12f44c0fb0e64530fad
Anthony Parra <anthonyparra62@gmail.com>
2021-02-10 10:30:39 +0100

    Modification de la taille des images background

---
+++ index.js
@@ -0,0 +1,17 @@
+/*const home = document.getElementById("home");
+const aboutMe = document.getElementById("aboutMe");
+const projects = document.getElementById("projects");
+const contact = document.getElementById("contact");
+
+contact.addEventListener('click', ()=>{
+    alert('HELLO');
+})
+home.addEventListener('click', ()=>{
+    alert('HELLO');
+})
+aboutMe.addEventListener('click', ()=>{
+    alert('HELLO');
+})
+projects.addEventListener('click', ()=>{
+    alert('HELLO');
+})*/
